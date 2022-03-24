98a460ace17317c2e7e446e5d138671a44df28cc
unknown <eras.projet@gmail.com>
2022-03-24 07:42:30 +0100

    Updated project

--- Jeu_de_données.sql
+++ Jeu_de_données.sql
@@ -1,4 +1,4 @@
-INSERT INTO Adresse VALUES (1, '131 Avenue de Lodève', NULL, '34080', 'Montpellier');
+INSERT INTO Adresse VALUES (1, '131 Avenue de Lodeve', NULL, '34080', 'Montpellier');
 INSERT INTO Adresse VALUES (2, '10 Rue Foch', NULL, '34080', 'Montpellier');
 INSERT INTO Adresse VALUES (3, '3 Impasse de la Fondue', NULL, '34080', 'Montpellier');
 INSERT INTO Adresse VALUES (4, '12 Rue des Acacias', NULL, '34080', 'Montpellier');
@@ -14,17 +14,17 @@ INSERT INTO Adresse VALUES (13, '5 Rue Fontenille', NULL, '34080', 'Montpellier'
 INSERT INTO Adresse VALUES (14, '7 Rue des Fraisiers', NULL, '34080', 'Montpellier');
 INSERT INTO Adresse VALUES (15, '17 Rue des Fraisiers', NULL, '34080', 'Montpellier');
 
-INSERT INTO Adresse VALUES (16, '3 Allée des Chênes', NULL, '66000', 'Perpignan');
+INSERT INTO Adresse VALUES (16, '3 Allee des Chenes', NULL, '66000', 'Perpignan');
 INSERT INTO Adresse VALUES (17, '33 Avenue Alfred Sauvy', NULL, '66000', 'Perpignan');
 INSERT INTO Adresse VALUES (18, '22 Avenue Bruxelles', NULL, '66000', 'Perpignan');
-INSERT INTO Adresse VALUES (19, '17 Avenue du Général de Gaulle', NULL, '66000', 'Perpignan');
+INSERT INTO Adresse VALUES (19, '17 Avenue du General de Gaulle', NULL, '66000', 'Perpignan');
 INSERT INTO Adresse VALUES (20, '5 Rue du Castillet', NULL, '66000', 'Perpignan');
 INSERT INTO Adresse VALUES (21, '11 Rue des Tuileries', NULL, '66000', 'Perpignan');
 INSERT INTO Adresse VALUES (22, '2 Rue du 14 Juillet', NULL, '66000', 'Perpignan');
-INSERT INTO Adresse VALUES (23, '5 Rue du Maréchal Foch', NULL, '66000', 'Perpignan');
-INSERT INTO Adresse VALUES (24, '6 Rue du Théâtre', NULL, '66000', 'Perpignan');
-INSERT INTO Adresse VALUES (25, '9 Boulevard Frédéric Mistral', NULL, '66000', 'Perpignan');
-INSERT INTO Adresse VALUES (26, '39 Boulevard Félix Mercader', NULL, '66000', 'Perpignan');
+INSERT INTO Adresse VALUES (23, '5 Rue du Marechal Foch', NULL, '66000', 'Perpignan');
+INSERT INTO Adresse VALUES (24, '6 Rue du Theatre', NULL, '66000', 'Perpignan');
+INSERT INTO Adresse VALUES (25, '9 Boulevard Frederic Mistral', NULL, '66000', 'Perpignan');
+INSERT INTO Adresse VALUES (26, '39 Boulevard Felix Mercader', NULL, '66000', 'Perpignan');
 INSERT INTO Adresse VALUES (27, '14 Rue du Rivage', NULL, '66000', 'Perpignan');
 INSERT INTO Adresse VALUES (28, '6 Rue des Embruns', NULL, '66000', 'Perpignan');
 INSERT INTO Adresse VALUES (29, '22 Rue de la Barre', NULL, '66000', 'Perpignan');
@@ -36,7 +36,7 @@ INSERT INTO Adresse VALUES (33, '1 Rue Santerre', NULL, '75012', 'Paris');
 INSERT INTO Adresse VALUES (34, '7 Rue Sauval', NULL, '75000', 'Paris');
 INSERT INTO Adresse VALUES (35, '12 Rue Gutenberg', NULL, '75015', 'Paris');
 INSERT INTO Adresse VALUES (36, '9 Rue Guynemer', NULL, '75006', 'Paris');
-INSERT INTO Adresse VALUES (37, '15 Rue Halévy', NULL, '75009', 'Paris');
+INSERT INTO Adresse VALUES (37, '15 Rue Halevy', NULL, '75009', 'Paris');
 INSERT INTO Adresse VALUES (38, '2 Rue Gustave Flaubert', NULL, '75017', 'Paris');
 INSERT INTO Adresse VALUES (39, '1 Rue Haxo', NULL, '75019', 'Paris');
 INSERT INTO Adresse VALUES (40, '32 Rue Henner', NULL, '75009', 'Paris');
@@ -44,7 +44,7 @@ INSERT INTO Adresse VALUES (41, '6 Rue Hassard', NULL, '75019', 'Paris');
 INSERT INTO Adresse VALUES (42, '2 Rue Jacquard', NULL, '75011', 'Paris');
 INSERT INTO Adresse VALUES (43, '16 Rue Houdon', NULL, '75018', 'Paris');
 INSERT INTO Adresse VALUES (44, '11 Rue Jacquemont', NULL, '75017', 'Paris');
-INSERT INTO Adresse VALUES (45, '11 Rue Hélène Brion', NULL, '75013', 'Paris');
+INSERT INTO Adresse VALUES (45, '11 Rue Helene Brion', NULL, '75013', 'Paris');
 
 
 
@@ -56,11 +56,11 @@ INSERT INTO Commerce VALUES (3, 'OC Pizza Paris', 31);
 
 INSERT INTO Client VALUES (1, 'Doe', 'John', STR_TO_DATE('17/12/1970', '%d/%m/%Y'), '0600000000', 'john.doe@mail.com', 'Testmdp', 2, 1);
 INSERT INTO Client VALUES (2, 'Doe', 'Jane', STR_TO_DATE('18/01/1971', '%d/%m/%Y'), '0600000001', 'jane.doe@mail.com', 'Testmdp', 3, 1);
-INSERT INTO Client VALUES (3, 'Martin', 'Céline', STR_TO_DATE('19/02/1972', '%d/%m/%Y'), '0600000002', 'celine.martin@mail.com', 'Testmdp', 4, 1);
+INSERT INTO Client VALUES (3, 'Martin', 'Celine', STR_TO_DATE('19/02/1972', '%d/%m/%Y'), '0600000002', 'celine.martin@mail.com', 'Testmdp', 4, 1);
 INSERT INTO Client VALUES (4, 'Bernard', 'Yannick', STR_TO_DATE('20/03/1973', '%d/%m/%Y'), '0600000003', 'yannick.bernard@mail.com', 'Testmdp', 5, 1);
 INSERT INTO Client VALUES (5, 'Thomas', 'Justin', STR_TO_DATE('21/04/1974', '%d/%m/%Y'), '0600000004', 'justin.thomas@mail.com', 'Testmdp', 6, 1);
 INSERT INTO Client VALUES (6, 'Petit', 'Marie', STR_TO_DATE('22/05/1975', '%d/%m/%Y'), '0600000005', 'marie.petit@mail.com', 'Testmdp', 7, 1);
-INSERT INTO Client VALUES (7, 'Robert', 'Clément', STR_TO_DATE('23/06/1976', '%d/%m/%Y'), '0600000006', 'clement.robert@mail.com', 'Testmdp', 8, 1);
+INSERT INTO Client VALUES (7, 'Robert', 'Clement', STR_TO_DATE('23/06/1976', '%d/%m/%Y'), '0600000006', 'clement.robert@mail.com', 'Testmdp', 8, 1);
 INSERT INTO Client VALUES (8, 'Richard', 'Paul', STR_TO_DATE('24/07/1977', '%d/%m/%Y'), '0600000007', 'paul.richard@mail.com', 'Testmdp', 9, 1);
 INSERT INTO Client VALUES (9, 'Durand', 'Ellie', STR_TO_DATE('25/08/1978', '%d/%m/%Y'), '0600000008', 'ellie.durand@mail.com', 'Testmdp', 10, 1);
 INSERT INTO Client VALUES (10, 'Dubois', 'Gabriel', STR_TO_DATE('26/09/1979', '%d/%m/%Y'), '0600000009', 'gabriel.dubois@mail.com', 'Testmdp', 11, 1);
@@ -73,10 +73,10 @@ INSERT INTO Client VALUES (15, 'Lefevre', 'Laure', STR_TO_DATE('31/03/1984', '%d
 INSERT INTO Client VALUES (16, 'Liroy', 'Camille', STR_TO_DATE('01/02/1985', '%d/%m/%Y'), '0600000015', 'camille.liroy@mail.com', 'Testmdp', 22, 2);
 INSERT INTO Client VALUES (17, 'Roux', 'Lilian', STR_TO_DATE('02/04/1986', '%d/%m/%Y'), '0600000016', 'lilian.roux@mail.com', 'Testmdp', 23, 2);
 INSERT INTO Client VALUES (18, 'David', 'Laurence', STR_TO_DATE('03/05/1987', '%d/%m/%Y'), '0600000017', 'laurence.david@mail.com', 'Testmdp', 24, 2);
-INSERT INTO Client VALUES (19, 'Bertrand', 'Jérome', STR_TO_DATE('04/06/1988', '%d/%m/%Y'), '0600000018', 'jerome.bertrand@mail.com', 'Testmdp', 25, 2);
+INSERT INTO Client VALUES (19, 'Bertrand', 'Jerome', STR_TO_DATE('04/06/1988', '%d/%m/%Y'), '0600000018', 'jerome.bertrand@mail.com', 'Testmdp', 25, 2);
 INSERT INTO Client VALUES (20, 'Morel', 'Patrice', STR_TO_DATE('05/07/1989', '%d/%m/%Y'), '0600000019', 'patrice.morel@mail.com', 'Testmdp', 26, 2);
 
-INSERT INTO Client VALUES (21, 'Fournier', 'Léo', STR_TO_DATE('06/08/1990', '%d/%m/%Y'), '0600000020', 'leo.fournier@mail.com', 'Testmdp', 32, 3);
+INSERT INTO Client VALUES (21, 'Fournier', 'Leo', STR_TO_DATE('06/08/1990', '%d/%m/%Y'), '0600000020', 'leo.fournier@mail.com', 'Testmdp', 32, 3);
 INSERT INTO Client VALUES (22, 'Girard', 'Raphael', STR_TO_DATE('07/09/1991', '%d/%m/%Y'), '0600000021', 'raphael.girard@mail.com', 'Testmdp', 33, 3);
 INSERT INTO Client VALUES (23, 'Bonnet', 'Arthur', STR_TO_DATE('08/10/1992', '%d/%m/%Y'), '0600000022', 'arthur.bonnet@mail.com', 'Testmdp', 34, 3);
 INSERT INTO Client VALUES (24, 'Dupont', 'Jules', STR_TO_DATE('09/11/1993', '%d/%m/%Y'), '0600000023', 'jules.dupont@mail.com', 'Testmdp', 35, 3);
@@ -90,8 +90,8 @@ INSERT INTO Client VALUES (30, 'Faure', 'Alice', STR_TO_DATE('15/05/1999', '%d/%
 
 
 INSERT INTO Ingredient VALUES (1, 'Sauce tomate');
-INSERT INTO Ingredient VALUES (2, 'Crème fraiche');
-INSERT INTO Ingredient VALUES (3, 'Chèvre');
+INSERT INTO Ingredient VALUES (2, 'Creme fraiche');
+INSERT INTO Ingredient VALUES (3, 'Chevre');
 INSERT INTO Ingredient VALUES (4, 'Mozzarella');
 INSERT INTO Ingredient VALUES (5, 'Emmental');
 INSERT INTO Ingredient VALUES (6, 'Roquefort');
@@ -99,7 +99,7 @@ INSERT INTO Ingredient VALUES (7, 'Oignon');
 INSERT INTO Ingredient VALUES (8, 'Olive');
 INSERT INTO Ingredient VALUES (9, 'Jambon');
 INSERT INTO Ingredient VALUES (10, 'Champignon');
-INSERT INTO Ingredient VALUES (11, 'Viande Hachée');
+INSERT INTO Ingredient VALUES (11, 'Viande Hachee');
 INSERT INTO Ingredient VALUES (12, 'Oeuf');
 INSERT INTO Ingredient VALUES (13, 'Pepperonie');
 INSERT INTO Ingredient VALUES (14, 'Lardon');
@@ -114,7 +114,7 @@ INSERT INTO Recette VALUES (1, 'Recette pizza 4 fromages');
 INSERT INTO Recette VALUES (2, 'Recette pizza royale');
 INSERT INTO Recette VALUES (3, 'Recette pizza montagnarde');
 INSERT INTO Recette VALUES (4, 'Recette pizza calzone');
-INSERT INTO Recette VALUES (5, 'Recette pizza chèvre miel');
+INSERT INTO Recette VALUES (5, 'Recette pizza chevre miel');
 INSERT INTO Recette VALUES (6, 'Recette pizza hawaienne');
 INSERT INTO Recette VALUES (7, 'Recette pizza pepperoni');
 
@@ -124,7 +124,7 @@ INSERT INTO Pizza VALUES (1, 'Pizza 4 fromages', 10.0, 1);
 INSERT INTO Pizza VALUES (2, 'Pizza royale', 10.0, 2);
 INSERT INTO Pizza VALUES (3, 'Pizza montagnarde', 10.0, 3);
 INSERT INTO Pizza VALUES (4, 'Pizza calzone', 10.0, 4);
-INSERT INTO Pizza VALUES (5, 'Pizza chèvre miel', 10.0, 5);
+INSERT INTO Pizza VALUES (5, 'Pizza chevre miel', 10.0, 5);
 INSERT INTO Pizza VALUES (6, 'Pizza hawaienne', 10.0, 6);
 INSERT INTO Pizza VALUES (7, 'Pizza pepperoni', 10.0, 7);
 
@@ -132,11 +132,11 @@ INSERT INTO Pizza VALUES (7, 'Pizza pepperoni', 10.0, 7);
 
 INSERT INTO Etat VALUES (1, 'PANIER');
 INSERT INTO Etat VALUES (2, 'PRIS EN COMPTE');
-INSERT INTO Etat VALUES (3, 'EN PRÉPARATION');
+INSERT INTO Etat VALUES (3, 'EN PREPARATION');
 INSERT INTO Etat VALUES (4, 'EN CUISSON');
-INSERT INTO Etat VALUES (5, 'PRÊTE');
+INSERT INTO Etat VALUES (5, 'PRETE');
 INSERT INTO Etat VALUES (6, 'EN LIVRAISON');
-INSERT INTO Etat VALUES (7, 'TERMINÉE');
+INSERT INTO Etat VALUES (7, 'TERMINEE');
 
 
 
@@ -186,7 +186,7 @@ INSERT INTO Commande VALUES (33, 20, 7, 3);
 
 
 
-INSERT INTO Role VALUES (1, 'Employé');
+INSERT INTO Role VALUES (1, 'Employe');
 INSERT INTO Role VALUES (2, 'Directeur du restaurant');
 
 
@@ -198,13 +198,13 @@ INSERT INTO Employe VALUES (4, 'Colin', 'Alice', STR_TO_DATE('20/03/1983', '%d/%
 
 INSERT INTO Employe VALUES (5, 'Olivier', 'Rose', STR_TO_DATE('21/04/1971', '%d/%m/%Y'), '0600000034', 'rose.olivier@mail.com', 'Testmdp', 2, 27, 2);
 INSERT INTO Employe VALUES (6, 'Pierre', 'Augustin', STR_TO_DATE('22/05/1985', '%d/%m/%Y'), '0600000035', 'augustin.pierre@mail.com', 'Testmdp', 1, 28, 2);
-INSERT INTO Employe VALUES (7, 'Rey', 'Léa', STR_TO_DATE('23/06/1988', '%d/%m/%Y'), '0600000036', 'lea.rey@mail.com', 'Testmdp', 1, 29, 2);
+INSERT INTO Employe VALUES (7, 'Rey', 'Lea', STR_TO_DATE('23/06/1988', '%d/%m/%Y'), '0600000036', 'lea.rey@mail.com', 'Testmdp', 1, 29, 2);
 INSERT INTO Employe VALUES (8, 'Payet', 'Antoine', STR_TO_DATE('24/07/1987', '%d/%m/%Y'), '0600000037', 'antoine.payet@mail.com', 'Testmdp', 1, 30, 2);
 
 INSERT INTO Employe VALUES (9, 'Lejeune', 'Valentin', STR_TO_DATE('25/08/1973', '%d/%m/%Y'), '0600000038', 'valentin.lejeune@mail.com', 'Testmdp', 2, 42, 3);
-INSERT INTO Employe VALUES (10, 'Barthelemy', 'Inès', STR_TO_DATE('26/09/1985', '%d/%m/%Y'), '0600000039', 'ines.barthelemy@mail.com', 'Testmdp', 1, 43, 3);
+INSERT INTO Employe VALUES (10, 'Barthelemy', 'Ines', STR_TO_DATE('26/09/1985', '%d/%m/%Y'), '0600000039', 'ines.barthelemy@mail.com', 'Testmdp', 1, 43, 3);
 INSERT INTO Employe VALUES (11, 'Cordier', 'Agathe', STR_TO_DATE('27/10/1986', '%d/%m/%Y'), '0600000040', 'agathe.cordier@mail.com', 'Testmdp', 1, 44, 3);
-INSERT INTO Employe VALUES (12, 'Chevalier', 'Léonie', STR_TO_DATE('28/11/1989', '%d/%m/%Y'), '0600000041', 'leonie.chevalier@mail.com', 'Testmdp', 1, 45, 3);
+INSERT INTO Employe VALUES (12, 'Chevalier', 'Leonie', STR_TO_DATE('28/11/1989', '%d/%m/%Y'), '0600000041', 'leonie.chevalier@mail.com', 'Testmdp', 1, 45, 3);
 
 
 
